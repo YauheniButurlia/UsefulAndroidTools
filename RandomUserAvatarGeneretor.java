edb8f7c25dc09ffe646739d8236e07f952604533
Yauheni <yauheni.buturlia@yahoo.com>
2019-04-09 18:59:55 +0300

    Add simple RandomUserAvatar Generator

---
+++ RandomUserAvatarGeneretor.java
@@ -0,0 +1,28 @@
+import java.net.*;
+import java.io.*;
+import javax.net.ssl.HttpsURLConnection;
+
+public class ImageDown {
+  public static void main(String[] args) throws Exception {
+    int amount = 1;
+    if(args.length != 0){
+      amount = Integer.parseInt(args[0]);
+    }
+    URL url = new URL("https://thispersondoesnotexist.com/image");
+    for(int i = 0; i < amount; i++){
+      HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+      connection.connect();
+      InputStream response = connection.getInputStream();
+      FileOutputStream out = new FileOutputStream("Photo" + String.valueOf(i) + ".jpg");
+      byte[] buffer = new byte[200000];
+      int c;
+      while((c = response.read(buffer)) != -1) {
+        out.write(buffer,0,c);
+      }
+      connection.disconnect();
+      response.close();
+      out.close();
+      Thread.sleep(1000);
+    }
+  }
+}
